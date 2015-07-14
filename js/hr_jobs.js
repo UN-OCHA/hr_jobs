f146d52b853faab47e5708d38fa054a59effdd31
MartinMaritim <trollmatt@gmail.com>
2015-07-14 16:20:35 +0300

    Revert "Added more functionality to the initial commit"
    This reverts commit bd5227cecbef3bfd265802d70990abacf3aac72f.

--- js/hr_jobs.js
+++ js/hr_jobs.js
@@ -2,44 +2,21 @@
 Drupal.behaviors.hrJobs = {
   attach: function (context, settings) {
 
-  Job = Backbone.Model.extend({
-    defaults: {
-      title: '',
-      url: '',
-    },
-  });
-
   JobsList = Backbone.Collection.extend({
 
-    model: Job,
-    //params: {},
     url: function() {
-      return 'http://api.rwlabs.org/v1/jobs?query[fields][]=country&fields[include][]=url&query[value]=' + settings.hr_jobs.operation;
+      return 'http://api.rwlabs.org/v1/jobs?query[fields][]=country&fields[include][]=url&query[value]=' + settings.hr_jobs.operation  + '&callback=?';
     },
 
     parse: function(response) {
       this.count = response.count;
       this.totalCount = response.totalCount;
-
-      var models = response.data ? response.data : {};
-      return _.map(models, function(model){
-        var fields = model.fields,
-        title = fields.title,
-        url = fields.url;
-
-        return (_.extend({}, fields, {title: title, url: url}));
-      }, this);
+      return response.data.fields;
     },
-
-    limit: 5,
-    skip: 0,
-    count: 0,
 
   });
 
     JobView = Backbone.View.extend({
-
-      router: null,
 
       clear: function() {
         this.$el.empty();
@@ -65,15 +42,14 @@ Drupal.behaviors.hrJobs = {
         initialize: function() {
             this.JobsList = new JobsList;
             this.JobsList.limit = this.numItems;
-            this.render();
         },
 
         loadResults: function() {
           var that = this;
           this.JobsList.fetch({
-            success: function (fields) {
+            success: function (jobs) {
               var template = _.template($('#jobs_list_view').html());
-              $('#jobs-list-table tbody').append(template({fields: fields.models}));
+              $('#jobs-list-table tbody').append(template({jobs: jobs.models}));
               that.finishedLoading();
             },
           });
@@ -83,11 +59,6 @@ Drupal.behaviors.hrJobs = {
           this.loading();
           this.currentPage = page;
           this.clear();
-          this.JobsList.skip = this.numItems * (page - 1);
-          this.loadResults();
-        },
-
-        render: function (model){
           this.loadResults();
         },
 
@@ -109,34 +80,7 @@ Drupal.behaviors.hrJobs = {
         },
     });
 
-    JobsRouter = Backbone.Router.extend({
-      routes: {
-        "table/:page" : "table",
-        "*actions": "defaultRoute",
-      },
-
-      tableView: new JobTableView({collection: JobsList, el: 'body'}),
-
-      initialize: function() {
-        this.tableView.router = this;
-      },
-
-      defaultRoute: function (actions) {
-        this.navigate('table/1', {trigger: true});
-      },
-
-      table: function(page) {
-        this.tableView.page(page);
-      },
-
-      navigateWithParams: function(url, params) {
-        this.navigate(url + '?' + $.param(params), {trigger: true});
-      },
-    });
     Backbone.history.start();
-    var jobs_router = new JobsRouter;
-
-    //Backbone.history.start();
 
   }
 }
